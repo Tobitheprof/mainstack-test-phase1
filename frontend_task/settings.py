cdf5e4e72423f2f9283961bb723112e6835d9c06
Tobi TheRevolutionary <tobiekundayo07@gmail.com>
2023-04-27 01:00:12 +0100

    Turned on debug

--- frontend_task/settings.py
+++ frontend_task/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-41g$1e&_b)+jl6a!fm5k5#s^xh#7zrdam)__jv!upioybf)cq2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
